576788ae1b8b638b1f873a9072c51e70ce3c6db7
Godoy <renanfgfg@gmail.com>
2022-01-15 16:57:11 -0300

    criando metodo adicionar funcionarios

--- src/main/java/com/gxdxy/projetospringreact/controller/FuncionarioController.java
+++ src/main/java/com/gxdxy/projetospringreact/controller/FuncionarioController.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +25,10 @@ public class FuncionarioController {
 	public List<Funcionario> listarFuncionarios (){
 		return funcionarioRepository.findAll();
 	}
+	
+	@PostMapping("/adicionarFuncionarios")
+	public Funcionario adicionarFuncionarios (@RequestBody Funcionario f) {
+		return funcionarioRepository.save(f);
+	}
+	
 }
