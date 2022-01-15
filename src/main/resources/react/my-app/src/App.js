3e200a40bb889918ce91431dbef56af74749d14f
Godoy <renanfgfg@gmail.com>
2022-01-15 03:16:37 -0300

    adicionando routers ao projeto

--- src/main/resources/react/my-app/src/App.js
+++ src/main/resources/react/my-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router';
 import ListarFuncionarioComponent from './components/ListarFuncionarioComponent';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
@@ -7,17 +9,19 @@ import HeaderComponent from './components/HeaderComponent';
 function App() {
   return (
     <div>
-
-      <HeaderComponent/>
-
-      <div className="container">
+      <Router>
         
-        <ListarFuncionarioComponent></ListarFuncionarioComponent>
-
-      </div>
-
-      <FooterComponent/>
-
+          <HeaderComponent/>
+            <div className="container">
+              <Routes> 
+                <Route path = "/" element = {<ListarFuncionarioComponent/>}></Route>
+                <Route path = "/listarFuncionarios" element = {<ListarFuncionarioComponent/>}></Route>
+                
+              </Routes>
+            </div>
+          <FooterComponent/>
+      
+      </Router>
     </div>
   );
 }
