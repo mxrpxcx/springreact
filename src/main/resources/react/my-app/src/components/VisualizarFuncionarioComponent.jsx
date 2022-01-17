ebd11ef2864fa4b13076d28bc1980921daadfe43
Godoy <renanfgfg@gmail.com>
2022-01-17 06:16:41 -0300

    tela mostrando dados do funcionario

--- src/main/resources/react/my-app/src/components/VisualizarFuncionarioComponent.jsx
+++ src/main/resources/react/my-app/src/components/VisualizarFuncionarioComponent.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { useNavigate } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import FuncionarioService from '../services/FuncionarioService';
 
 class VisualizarFuncionarioComponent extends Component {
 
@@ -6,18 +9,56 @@ class VisualizarFuncionarioComponent extends Component {
         super(props)
 
         this.state = {
-            
+            id: '',
+            nome: '',
+            sobrenome: '',
+            email: ''
         }
 
+    }
+
+    componentDidMount(){
+        FuncionarioService.getFuncionarioId(this.props.id).then((res)=>{
+            let funcionario = res.data;
+            this.setState({
+                nome: funcionario.nome,
+                sobrenome: funcionario.sobrenome,
+                email: funcionario.email
+            })
+        });
     }
 
     render() {
         return (
             <div>
-                <h2> Visualizar dados do funcionário </h2>
+                <div className='card col-md-6 offset-md-3'>
+                    <h3 className='text-center'> Visualizar dados do funcionário </h3>
+                    <div className='card-body'>
+                        <div className='row'>
+                            <h6 className='font-weight-bold'>Nome</h6>
+                            <div>{this.state.nome}</div>
+                        </div>
+                        <br/>
+                        <div className='row'>
+                            <h6 className='font-weight-bold'>Sobrenome</h6>
+                            <div>{this.state.sobrenome}</div>
+                        </div>
+                        <br/>
+                        <div className='row'>
+                            <h6 className='font-weight-bold'>Email</h6> 
+                            <div>{this.state.email}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default VisualizarFuncionarioComponent;
+function WithNavigate(props){
+    const {id}  = useParams();
+    const navigate = useNavigate();
+    return <VisualizarFuncionarioComponent {...props} navigate={navigate} id={id}/>
+}
+ 
+export default WithNavigate;
